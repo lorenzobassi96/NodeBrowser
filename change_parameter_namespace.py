a2f8bc266a858d6a324523faea19d813395ab49b
lorenzobassi96 <44102822+lorenzobassi96@users.noreply.github.com>
2021-06-03 11:21:32 +0200

    Update change_parameter_namespace.py

--- change_parameter_namespace.py
+++ change_parameter_namespace.py
@@ -1,4 +1,17 @@
 #script.py (ip_server) (porta) (indice) (nuovo_parametro)
+import os
+import socket
+import json
+import requests
+import datetime
+import subprocess
+import threading
+import uuid
+from random import choice
+from string import ascii_uppercase
+import time
+from flask import Flask, Response, request, abort, jsonify
+
 import time
 from multiprocessing import Process
 import signal
@@ -6,15 +19,44 @@ from node import *
 from opcua import Client
 import sys
 
+MEC_SERVICE_MGMT="mec_service_mgmt/v1"
+
+mec_base = str(sys.argv[1])
+
+def get_all_services():
+    global mec_base
+    out = ''
+    query_base = "{}/{}/services".format(
+            mec_base,
+            MEC_SERVICE_MGMT,
+    )
+
+    r = requests.get(query_base)
+
+    return json.loads(r.text)
+
 def do_actions():
-    """
-    Function that should timeout after 5 seconds. It simply prints a number and waits 1 second.
-    :return:
-    """
-    hostname = str(sys.argv[1])
-    port = str(sys.argv[2])
-    index = int(sys.argv[3])
-    param = str(sys.argv[4])
+    endpoint = {}
+    
+    try:
+        srvs = get_all_services()
+        for s in srvs:
+            if s['transportInfo']['type'] == 'MB_TOPIC_BASED' and s['transportInfo']['protocol'] == 'OPCUA':   
+                endpoint=s['transportInfo']['endpoint']['addresses']
+                opc_ua_server_service_id = s['serInstanceId']                                               
+        if not endpoint:
+            abort(404, description="No OPCUA endpoint found")
+                
+    except:
+        abort(500, description="Unable to retrive service list")
+    
+    qos = 0
+    rate = 1000
+    hostname = endpoint[0]['host']
+    port = endpoint[0]['port']
+    
+    index = int(sys.argv[2])
+    param = str(sys.argv[3])
 
     domain = "opc.tcp://"
     ddd = ":"
